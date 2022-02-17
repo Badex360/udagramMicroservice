09e5eeb6c0f82cc2d7cebbb53a7160e0bcf12ce9
unknown <king.ibadmad@gmail.com>
2022-02-17 18:23:01 +0100

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME= username
+export POSTGRES_PASSWORD= password
+export POSTGRES_HOST= postgres endpoint
+export POSTGRES_DB= name of db
+export AWS_BUCKET= aws bucket name
+export AWS_REGION= name of region 
+export AWS_PROFILE=profile name
+export JWT_SECRET= jwt secret
+export URL=http://localhost:8100
